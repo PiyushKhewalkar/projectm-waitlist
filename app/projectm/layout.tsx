9c772654ea4c8a73809b5176b7b096944c249ebc
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-04-08 10:46:54 +0530

    analytics

--- app/projectm/layout.tsx
+++ app/projectm/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 
+import { Analytics } from "@vercel/analytics/react"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
